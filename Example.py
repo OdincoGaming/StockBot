722d909661de89d398626a94e2aaaf80db0c4d3a
OdincoGaming <45342056+OdincoGaming@users.noreply.github.com>
2019-12-04 12:54:22 -0600

    Fully Functional First Commit
    kinda rushed the initial commit.

--- Example.py
+++ Example.py
@@ -5,16 +5,41 @@ import time
 ############################################################################################
 #FUNCTIONS
 
-def GetStockShift(symbol, timeIncrement, timePeriod):
-    # Get daily price data for AAPL over the last 5 minutes.
+def GetPriceShift(symbol, timeIncrement, timePeriod):
+    # Get daily price data for stock over the last 5 minutes.
     barset = api.get_barset(symbol, timeIncrement, limit=timePeriod)
-    aapl_bars = barset[symbol]
+    bars = barset[symbol]
 
-    # See how much AAPL moved in that timeframe.
-    week_open = aapl_bars[0].o
-    week_close = aapl_bars[-1].c
+    # See how muc stock moved in that timeframe.
+    week_open = bars[0].o
+    week_close = bars[-1].c
     percent_change = (week_close - week_open) / week_open * 100
     return(percent_change)
+
+def NumberOwned(symbol):
+    numOwned = 0
+    for position in portfolio:
+        if position.symbol == symbol:
+            numOwned += 1
+    return numOwned
+
+def BuyAtMarket(symbol, qty, type, tif,):
+    api.submit_order(
+    symbol=symbol,
+    qty=qty,
+    side='buy',
+    type=type,
+    time_in_force=tif
+)
+
+def SellAtMarket(symbol, qty, type, tif):
+    api.submit_order(
+    symbol=symbol,
+    qty=qty,
+    side='sell',
+    type=type,
+    time_in_force=tif,
+)
 
 #FUNCTIONS
 ############################################################################################
@@ -30,22 +55,31 @@ api = tradeapi.REST(API_KEY, API_SECRET, APCA_API_BASE_URL, 'v2')
 #only search for NASDAQ stock (just cuz theres alot of companies selling stock lol)
 active_assets = api.list_assets(status='active')
 nasdaq_assets = [a for a in active_assets if a.exchange == 'NASDAQ']
+portfolio = api.list_positions()
 
 #check change in price over the last 5 days of each active 
 #stock available for trade through NASDAQ
 i = 0
 for asset in nasdaq_assets:
+    numOwned = NumberOwned(asset.symbol)
     if(i >=74 ):
         #keeps the bot from making too many API calls and stalling out
         print("Zzz")
         time.sleep(30)
         print("Good morning!")
         i = 0
-    change = GetStockShift(asset.symbol, 'day', 5)
+    change = GetPriceShift(asset.symbol, 'day', 5)
     if (change >= 3):
         print("sell " + asset.symbol)
+        if numOwned >= 1:
+            if asset.tradable:
+                SellAtMarket(asset.symbol, 1, 'market', 'gtc')
     elif(change >= -3 and change < 3):
         print("hold " + asset.symbol)
+        #update how many times held on stock object
     else:
         print("buy " + asset.symbol)
-    i += 1
+        if asset.tradable:
+            BuyAtMarket(asset.symbol, 1, 'market', 'gtc')
+        #when purchase complete, make a stock object to keep track of it and its data
+        i += 1
--- README.md
+++ README.md
@@ -12,3 +12,30 @@ after installing your preferred version of python
 open command prompt and type "pip install alpaca-trade-api" then hit enter, that should be all you need to run the Example script
 in command prompt navigate to folder directory ("cd yourDirectoryHere" + enter) and type "python Example.py" then hit enter to watch it go!
 
+
+LEARNAGE :
+-------------------------------------------
+
+Alpaca API, access to the stock market
+--------------------------------------
+https://docs.alpaca.markets/api-documentation/
+https://docs.alpaca.markets/api-documentation/how-to/
+https://docs.alpaca.markets/get-started-with-alpaca/tutorial-videos/
+https://www.youtube.com/watch?v=GsGeLHTOGAg
+https://www.youtube.com/watch?v=SIBr28sJFc8
+https://www.youtube.com/watch?v=jzIJ-0rDvCo
+
+Playing With Data!
+------------------
+https://www.youtube.com/watch?v=QAkOnV1-lIg
+https://www.youtube.com/watch?v=d2kXmWzfS0w
+https://www.youtube.com/watch?v=2BrpKpWwT2A (his whole series)
+
+Machine Learning w/ Stock Market Data
+-------------------------------------
+https://towardsdatascience.com/aifortrading-2edd6fac689d
+https://towardsdatascience.com/getting-rich-quick-with-machine-learning-and-stock-market-predictions-696802da94fe
+https://blog.alpaca.markets/blog/2018/7/24/building-a-simple-stock-trading-bot-by-using-brokerapi
+
+
+
